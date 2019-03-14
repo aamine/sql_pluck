508c754275bf5a218848a4e614c62337df55e94b
Minero Aoki <minero-aoki@cookpad.com>
2019-03-14 15:06:47 +0900

    Support Rails 5.x

--- lib/sql_pluck.rb
+++ lib/sql_pluck.rb
@@ -14,20 +14,7 @@ module SQLPluck
   # like .pluck method.
   #
   def pluck_by_sql(sql, binds = [])
-    rs = connection.select_all(sanitize_sql(sql), "#{name} Load", binds)
-    types = rs.column_types
-    rs.map {|record| cast_record(record, types) }
-  end
-
-  private
-
-  def cast_record(record, types)
-    record.map {|name, string| types[name].type_cast(string) }
-  end
-end
-
-if defined?(ActiveRecord::Type)
-  class ActiveRecord::Type::Value   # reopen
-    public :type_cast
+    result = connection.select_all(sanitize_sql(sql), "#{name} Load", binds)
+    result.cast_values(result.column_types)
   end
 end
